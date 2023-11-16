917eea794cf516abb449efa5d6215cc9a791b101
Florian Lemmer <flolema98@gmail.com>
2023-11-16 14:48:30 +0100

    initial fixes

--- src/main/kotlin/thkoeln/dungeon/restadapter/PlanetDto.kt
+++ src/main/kotlin/thkoeln/dungeon/restadapter/PlanetDto.kt
@@ -1,2 +1,30 @@
 package thkoeln.dungeon.restadapter
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import thkoeln.dungeon.domainprimitives.MineableResource
+import thkoeln.dungeon.domainprimitives.MineableResourceType
+import thkoeln.dungeon.domainprimitives.MovementDifficulty
+import thkoeln.dungeon.planet.domain.Planet
+import java.util.*
+
+data class PlanetDto(
+    val planetId: UUID,
+    val resourceType: MineableResourceType?,
+    val movementDifficulty: Int,
+    val gameWorldId: UUID,
+
+    var northNeighbour: Planet,
+
+    var eastNeighbour: Planet,
+
+    var southNeighbour: Planet,
+
+    var westNeighbour: Planet,
+
+    var mineableResource: MineableResource,
+
+    @JsonProperty("resourceType")
+    var _resourceType: MineableResourceType,
+
+) {
+}
